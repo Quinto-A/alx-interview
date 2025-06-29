654eab5c7f628564a6d726857feb46989c4fb110
Quinto-A <acquinoquinto5@gmail.com>
2025-06-29 18:21:36 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,12 +17,19 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
+    coins.sort()
+
+    max_coins_needed = total + 1
+
+    dp = [max_coins_needed] * (total + 1)
     dp[0] = 0
 
     for i in range(1, total + 1):
         for coin in coins:
-            if i - coin >= 0 and dp[i - coin] != float('inf'):
+            if coin > i:
+                break
+
+            if dp[i - coin] != max_coins_needed:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != max_coins_needed else -1
